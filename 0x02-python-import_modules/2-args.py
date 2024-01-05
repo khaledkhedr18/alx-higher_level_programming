9454b8e470d61efff465780d4472e7289e5c7445
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-01-05 03:08:21 +0200

    ss

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-
-import sys
-
+#  Prints the number of and the list of its arguments
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print("0 arguments.")
+    import sys
+
+    arg = sys.argv
+    size = len(arg) - 1
+
+    if size > 1:
+        print("{} arguments:".format(size))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, arg[i]))
+
+    elif size == 0:
+        print("{} arguments.".format(size))
+
     else:
-        print(f"{len(sys.argv) - 1} argument{'s' if len(sys.argv) > 2 else ''}: ", end="")
-        print(*sys.argv[1:], sep=", ")
+        print("{} argument:".format(size))
+        print("{}: {}".format(size, arg[1]))
