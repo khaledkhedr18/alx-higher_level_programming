fc1d30f3fe0afdeb8d19923f426bfeced87e0383
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-01-06 04:53:59 +0200

    ss

--- trashtest/test.py
+++ trashtest/test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = " ".join(str.split(",")[2].split()[3:5])
+str = str[39:65] + str[-22:-18] + str[:6]
 print(str)
