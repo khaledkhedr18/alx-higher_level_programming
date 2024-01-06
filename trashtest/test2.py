eb45c80d3f7b123bce6fd94cb74da13586cc4860
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-01-06 04:10:56 +0200

    ss

--- trashtest/test2.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-date = datetime(2024, 1, 6)
-balance = input("Please input the balance: ")
-fnum = float(balance)
-sep = "_"
-
-print(f"Your balance is: {fnum:{sep}.2f} and the date is: {date:%d/%m/%Y}")
