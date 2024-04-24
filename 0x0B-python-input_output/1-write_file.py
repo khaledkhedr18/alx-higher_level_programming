c2886f1a47bbc957bf79b89548f8c70e72d285d3
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-04-24 06:27:13 +0200

    ss

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,10 +4,10 @@
 def write_file(filename="", text=""):
     """ A function that writes into a file
     Args:
-    filename (str): The name of the file to write to.
-    text (str): The text to write into the file.
+        filename (str): The name of the file to write to.
+        text (str): The text to write into the file.
     Returns:
-    The number of bytes written
+        The number of bytes written
     """
     with open(filename, "w", encoding="utf-8") as f:
     f.write(text)
