8771a6b3a706a2452a890687900f64418cbc15d7
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-04-24 06:26:52 +0200

    ss

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,7 +7,7 @@ def write_file(filename="", text=""):
     filename (str): The name of the file to write to.
     text (str): The text to write into the file.
     Returns:
-    The number of bytes written    
+    The number of bytes written
     """
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(text)
+    f.write(text)
