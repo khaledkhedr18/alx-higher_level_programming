2c1d5c5c64b3fed1cf1d6185b45ab77afabeaf33
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-04-27 23:53:09 +0300

    0x0B

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this function returns the json representation of an object(string)
+"""
+import json
+
+
+def to_json_string(my_obj):
+	""" 
+	this function returns the json representation of an object(string)
+    """
+	return json.dumps(my_obj)
