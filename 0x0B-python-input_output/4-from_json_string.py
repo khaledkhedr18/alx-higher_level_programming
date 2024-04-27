daef6c033e70590c07e0d0583bd3fa0d8a652823
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-04-27 23:56:57 +0300

    0x0B

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+    this function returns an object (python data structure) represented by a JSON string
+"""
+
+
+def from_json_string(json_string):
+    """
+    this function returns an object (python data structure) represented by a JSON string
+    """
+    return json.loads(json_string)
