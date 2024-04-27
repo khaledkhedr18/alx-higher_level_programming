b88a301be0eddd287c10f68e2df2bee0a3858092
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-04-27 23:57:25 +0300

    0x0B

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import json
 """
-    this function returns an object (python data structure) represented by a JSON string
+    this function returns an object (python data structure)
+    represented by a JSON string
 """
 
 
 def from_json_string(json_string):
     """
-    this function returns an object (python data structure) represented by a JSON string
+    this function returns an object (python data structure)
+    represented by a JSON string
     """
     return json.loads(json_string)
