00817344857d0740195d75ee35a344ddbbd71507
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-04-27 21:14:55 +0300

    Update 1.py

--- trashtest/i/o py/1.py
+++ trashtest/i/o py/1.py
@@ -1,5 +1,7 @@
 import json
 
-x = [1, "khaled khedr", "student"]
-print(json.dumps(x))
+jsonnn = json.dumps([1, 2, 3, {'4': 5}, {'5': 6}], separators=(',', ': '))
 
+print(jsonnn)
+
+khaledjson = json.loads()
